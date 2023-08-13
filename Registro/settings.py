a9ba34dd24b14dfe0d5d6b53681a235642797ec4
Brandon <bg4710699@gmail.com>
2023-08-13 13:46:20 -0600

    configuracion allowed_host2

--- Registro/settings.py
+++ Registro/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-$&p(1tt$ui9c!mve!yu9nb-bp%4ao(j%79dqhuiejln81@648y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = 'servicios-municipales-alcaldia.onrender.com'
 
 
 # Application definition
