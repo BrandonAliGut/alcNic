44fbd8f567adfd2e87300ec761b7d5ff74a2470f
Brandon <bg4710699@gmail.com>
2023-08-13 13:28:22 -0600

    configuracion allowed_host

--- Registro/settings.py
+++ Registro/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$&p(1tt$ui9c!mve!yu9nb-bp%4ao(j%79dqhuiejln81@648y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
